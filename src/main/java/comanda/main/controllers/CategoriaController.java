893e59e2a21105b4faa22a74dcca21d90e294177
Rodrigo Ezequiel Miliano <rodrigomiliano16@gmail.com>
2022-10-10 23:37:37 -0300

    prueba correccion error crossorigin

--- src/main/java/comanda/main/controllers/CategoriaController.java
+++ src/main/java/comanda/main/controllers/CategoriaController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,6 +29,7 @@ public class CategoriaController extends ComandaApp implements ObjectController<
 		this.categoriaService = categoriaService;
 	}
 	
+	@CrossOrigin(origins = "http://localhost:3000")
 	@Override
 	@GetMapping(path = "/v1/categorias")
 	public List<CategoriaDTO> getAll() {
